8c9c460a106fe081b4dc302cb6821d58084e3374
Paola Posada <apoyodesarrolloingenieria4@udea.edu.co>
2021-01-13 19:00:56 -0500

    No se permiten añadir mas de 400 caracteres y se añade la imagen como background

--- src/app/pages/publisher/templates-cards/components/modal/modal.component.ts
+++ src/app/pages/publisher/templates-cards/components/modal/modal.component.ts
@@ -25,11 +25,13 @@ export class ModalComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<any>();
   itemImages: FileUpload[] = [];
   imageSrc: string = null;
+  imgB64: string = null;
   isOverDrop = false;
   mycontent: string;
   config: any;
   htmlContentWithoutStyles = '';
   formdata: FormGroup;
+  imgBase64: string = null;
 
   constructor(
     private dialogRef: MatDialogRef<ModalMailUsersComponent>,
@@ -37,16 +39,20 @@ export class ModalComponent implements OnInit, OnDestroy {
     private formBuilder: FormBuilder,
     private themeSwitcherController: ThemeSwitcherControllerService
   ) {
-    this.mycontent = `Hola&nbsp;<span><b><font color="#e74c3c">&lt;Nombre&gt;</font></b>&nbsp;en ésta&nbsp;<b><font color="#16a085">&lt;Fecha&gt;</font></b>&nbsp;la Universidad de Antioquia le desea un feliz cumpleaños 🥳.</span>`;
+    this.mycontent = `<div id="imgb">Hola&nbsp;<span><b><font color="#e74c3c">&lt;Nombre&gt;</font></b>&nbsp;en ésta&nbsp;<b><font color="#16a085">&lt;Fecha&gt;</font></b>&nbsp;la Universidad de Antioquia le desea un feliz cumpleaños 🥳.</span>
+  </div>`;
     this.htmlContentWithoutStyles = this.mycontent;
   }
 
   quitImage() {
     this.itemImages[0] = null;
     this.imageSrc = null;
+    const imagen = document.getElementById('imgb');
+    imagen.style.backgroundImage = `url(${this.imageSrc})`;
   }
 
   resetEditor() {
+    console.log(this.imageSrc);
     this.mycontent = '';
     this.htmlContentWithoutStyles = '';
     this.joditEditor.resetEditor();
@@ -62,9 +68,27 @@ export class ModalComponent implements OnInit, OnDestroy {
     }
   }
 
+  public picked(event) {
+    const file: File = event.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = this._handleReaderLoaded.bind(this);
+    reader.readAsDataURL(file);
+  }
+
+  _handleReaderLoaded(e) {
+    const reader = e.target;
+    this.imgBase64 = reader.result;
+    const imagen = document.getElementById('imgb');
+    imagen.style.backgroundImage = `url(${this.imageSrc})`;
+    imagen.style.backgroundRepeat = 'no-repeat';
+    imagen.style.backgroundPosition = 'center';
+    imagen.style.backgroundSize = 'cover';
+    imagen.style.height = '500px';
+  }
+
   addName() {
     this.mycontent = this.mycontent.substring(0, this.mycontent.length).concat(`<span id="name"><b><font color="#e74c3c">&lt;Nombre&gt;</font></b>&nbsp;</span>`);
-    // this.joditEditor.element.innerHTML = `<span id="name"><b><font color="#e74c3c">&lt;Nombre&gt;</font></b>&nbsp;</span>`;
+    //this.joditEditor.element.innerHTML = `<span id="name"><b><font color="#e74c3c">&lt;Nombre&gt;</font></b>&nbsp;</span>`;
   }
 
   addDate() {
@@ -98,7 +122,12 @@ export class ModalComponent implements OnInit, OnDestroy {
       },
       language: 'es',
       theme: 'default',
-      enter: 'DIV'
+      limitChars: 400,
+      toolbarButtonSize: 'small',
+      showXPathInStatusbar: false,
+      toolbarAdaptive: true,
+      disablePlugins: 'enter,table,about,delete,clean-html,wrap-text-nodes,placeholder,iframe,video,file,print,table-keyboard-navigation,select-cells,resize-cells'
+
     };
     this.themeSwitcherController.themeClass$
       .pipe(takeUntil(this.destroy$))
