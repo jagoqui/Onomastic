99dbcc6949620a87df4a2b900bc16dfb97f8d276
Paola Posada <apoyodesarrolloingenieria4@udea.edu.co>
2021-02-12 12:52:15 -0500

    Actualizacion front eventos

--- src/app/pages/publisher/events-day/components/modal-event-day/modal-event-day.component.ts
+++ src/app/pages/publisher/events-day/components/modal-event-day/modal-event-day.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import {
   TemplateCardsService,
@@ -13,10 +14,15 @@ import { Plantilla } from '@app/shared/models/template-card.model';
 export class ModalEventDayComponent implements OnInit {
   cards: Plantilla[] = [];
   sidenavOpened = false;
+  form: FormGroup;
+  maxListsConfig = {
+    condition: 4
+  };
 
   constructor(
     private dialogRef: MatDialogRef<ModalEventDayComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
+    private fb: FormBuilder,
     private templateCardsSevice: TemplateCardsService
   ) { }
 
@@ -24,6 +30,36 @@ export class ModalEventDayComponent implements OnInit {
     this.sidenavOpened = true;
     this.templateCardsSevice.getAllCards().subscribe(cards => this.cards = cards);
   }
+
+  getConditionsSize(): number {
+    return this.condition.length;
+  }
+
+  onShowAddCondition(iterator: number): boolean {
+    return (iterator === (this.getConditionsSize() - 1) && (this.getConditionsSize() < this.maxListsConfig.condition));
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      condition: this.fb.array([])
+    });
+  }
+
+  get condition(): FormArray {
+    return this.form.get('condition') as FormArray;
+  }
+
+  addCondition() {
+    const cond = this.fb.group({
+      condition: new FormControl(''),
+    });
+    this.condition.push(cond);
+  }
+
+  removeCondition(indice: number) {
+    this.condition.removeAt(indice);
+  }
+
 
   onClose(close?: boolean): void {
     if (close ? close : confirm('No ha guardado los cambios, desea salir?')) {
@@ -35,5 +71,7 @@ export class ModalEventDayComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.createForm();
+    this.addCondition();
   }
 }
