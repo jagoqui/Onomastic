114a1d0c6a3585cd237709b339bb3d311f396760
Jaidiver Gómez <jaidiver.gomez@udea.edu.co>
2021-02-22 10:20:26 -0500

    Se agrega lista de plantillas a la vista de plantillas.

--- src/app/pages/publisher/events-day/components/modal-event-day/modal-event-day.component.ts
+++ src/app/pages/publisher/events-day/components/modal-event-day/modal-event-day.component.ts
@@ -1,10 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { SafeHtml } from '@angular/platform-browser';
 import {
   TemplateCardsService,
 } from '@app/pages/publisher/services/template-cards.service';
-import { Plantilla } from '@app/shared/models/template-card.model';
+import { Plantilla } from '@shared/models/template-card.model';
+import { DomSanitizerService } from '@shared/services/dom-sanitizer.service';
 
 @Component({
   selector: 'app-modal',
@@ -20,11 +21,11 @@ export class ModalEventDayComponent implements OnInit {
     private dialogRef: MatDialogRef<ModalEventDayComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private templateCardsSevice: TemplateCardsService,
-    private sanitizer: DomSanitizer
+    private domSanitazerSvc: DomSanitizerService
   ) { }
 
   sanatizeHTML(cardText: string): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(cardText);
+    return this.domSanitazerSvc.sanatizeHTML(cardText);
   }
 
   loadCards(onChange?: boolean) {
@@ -45,6 +46,7 @@ export class ModalEventDayComponent implements OnInit {
       this.sidenavOpened = false;
     }
   }
+
   onClose(close?: boolean): void {
     if (close ? close : confirm('No ha guardado los cambios, desea salir?')) {
       this.dialogRef.close();
--- src/app/pages/publisher/templates-cards/templates-cards.component.ts
+++ src/app/pages/publisher/templates-cards/templates-cards.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { SafeHtml } from '@angular/platform-browser';
+import { Plantilla } from '@app/shared/models/template-card.model';
+import {
+  DomSanitizerService,
+} from '@app/shared/services/dom-sanitizer.service';
+import { LoaderService } from '@app/shared/services/loader.service';
 
+import { TemplateCardsService } from '../services/template-cards.service';
 import {
   ModalTemplateCardsComponent,
 } from './components/modal/modal-template-cards.component';
@@ -10,11 +17,18 @@ import {
   templateUrl: './templates-cards.component.html',
   styleUrls: ['./templates-cards.component.scss']
 })
-export class TemplatesCardsComponent implements OnInit {
+export class TemplatesCardsComponent implements OnInit, AfterViewInit {
+  cards: Plantilla[];
 
-  constructor(private dialog: MatDialog) { }
+  constructor(
+    private dialog: MatDialog,
+    private domSanitizerSvc: DomSanitizerService,
+    private templateCardsSevice: TemplateCardsService,
+    public loaderSvc: LoaderService
+  ) { }
 
-  ngOnInit(): void {
+  sanatizeHTML(card: string): SafeHtml {
+    return this.domSanitizerSvc.sanatizeHTML(card);
   }
 
   onOpenModal(): void {
@@ -27,4 +41,18 @@ export class TemplatesCardsComponent implements OnInit {
       data: { title: 'Nueva tarjeta' },
     });
   }
+
+  onSelectCard(card: Plantilla): void { }
+
+  ngAfterViewInit(): void {
+    this.templateCardsSevice.getAllCards().subscribe(cards => {
+      if (cards) {
+        this.cards = cards;
+        console.log(this.cards);
+
+      }
+    });
+  }
+
+  ngOnInit(): void { }
 }
