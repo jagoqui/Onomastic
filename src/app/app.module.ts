99d961f683e1628d0ab51b543b803351017a7bed
Jaidiver Gómez <jaidiver.gomez@udea.edu.co>
2021-04-12 18:22:22 -0500

    Se agrega location strategy para las rutas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  PageNotFoundComponent,
-} from '@shared/components/page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from '@shared/components/page-not-found/page-not-found.component';
 import { InterceptorService } from '@shared/interceptors/interceptor.service';
 import { SharedModule } from '@shared/shared.module';
 import { QuicklinkModule } from 'ngx-quicklink';
@@ -12,6 +10,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -32,6 +31,10 @@ import { AppComponent } from './app.component';
       provide: HTTP_INTERCEPTORS,
       useClass: InterceptorService,
       multi: true
+    },
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
     }
   ],
   bootstrap: [AppComponent]
