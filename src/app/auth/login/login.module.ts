d7c25df4aa44c25d6f10f56e8af9e84617cfeaaf
Jaidiver Gómez <jaidiver.gomez@udea.edu.co>
2021-04-11 20:08:58 -0500

    Se repara fallo con la importación de ngxRecaptcha.

--- src/app/auth/login/login.module.ts
+++ src/app/auth/login/login.module.ts
@@ -1,4 +1,5 @@
 import {NgModule} from '@angular/core';
+import {NgxSpinnerModule} from 'ngx-spinner';
 import {SharedModule} from 'src/app/shared/shared.module';
 
 import {LoginRoutingModule} from './login-routing.module';
@@ -9,6 +10,7 @@ import {LoginComponent} from './login.component';
   declarations: [LoginComponent],
   imports: [
     LoginRoutingModule,
+    NgxSpinnerModule,
     SharedModule
   ]
 })
