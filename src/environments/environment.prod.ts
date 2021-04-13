160b5af1822f8af904aa22ac5d4288798186d206
Jaidiver Gómez <jaidiver.gomez@udea.edu.co>
2021-04-13 12:36:22 -0500

    Se actualiza puerto de produción en el enviroment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://wsingenieria.udea.edu.co:8091/onomastico',
+  apiUrl: 'http://wsingenieria.udea.edu.co:8094/onomastico',
   recaptchaKey: '6Le7wOIZAAAAAL4OZ9W0B-Xju1nca9uRiS02Iazx',
-  uploadImagesUriServer:'http://wsingenieria.udea.edu.co:8091/onomastico/upload',
-  downloadImagesUriServer:'http://wsingenieria.udea.edu.co:8091/onomastico/images'
+  uploadImagesUriServer:'http://wsingenieria.udea.edu.co:8094/onomastico/upload',
+  downloadImagesUriServer:'http://wsingenieria.udea.edu.co:8094/onomastico/images'
 };
