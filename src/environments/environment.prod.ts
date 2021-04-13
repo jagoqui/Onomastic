17081e015b1dece1263103bfcb5bfb6ae096d147
Jaidiver Gómez <jaidiver.gomez@udea.edu.co>
2021-04-13 13:09:39 -0500

    Se agrega https a las peticiones de la api.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://wsingenieria.udea.edu.co:8094/onomastico',
+  apiUrl: 'https://wsingenieria.udea.edu.co:8094/onomastico',
   recaptchaKey: '6Le7wOIZAAAAAL4OZ9W0B-Xju1nca9uRiS02Iazx',
-  uploadImagesUriServer:'http://wsingenieria.udea.edu.co:8094/onomastico/upload',
-  downloadImagesUriServer:'http://wsingenieria.udea.edu.co:8094/onomastico/images'
+  uploadImagesUriServer:'https://wsingenieria.udea.edu.co:8094/onomastico/upload',
+  downloadImagesUriServer:'https://wsingenieria.udea.edu.co:8094/onomastico/images'
 };
