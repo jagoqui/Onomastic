e617082c7c38502327f30dbbf235bd1943b528d9
Jaidiver Gómez <jaidiver.gomez@udea.edu.co>
2021-04-22 01:18:02 -0500

    Se formatea enviroment de prod.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,3 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://wsingenieria.udea.edu.co:8094/onomastico',
-  recaptchaKey: '6Le7wOIZAAAAAL4OZ9W0B-Xju1nca9uRiS02Iazx',
-  uploadImagesUriServer:'https://wsingenieria.udea.edu.co:8094/onomastico/upload',
-  downloadImagesUriServer:'https://wsingenieria.udea.edu.co:8094/onomastico/images'
+  production: false
 };
