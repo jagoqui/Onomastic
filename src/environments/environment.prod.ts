ad641a231ef3736ef7796f4951852e8f8e0aa2d2
Jaidiver Gómez <jaidiver.gomez@udea.edu.co>
2021-04-22 01:22:46 -0500

    Se agregan rutas de prod en el enviroment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://wsingenieria.udea.edu.co:8094/onomastico',
+  recaptchaKey: '6Le7wOIZAAAAAL4OZ9W0B-Xju1nca9uRiS02Iazx',
+  uploadImagesUriServer: 'https://wsingenieria.udea.edu.co:8094/onomastico/upload',
+  downloadImagesUriServer: 'https://wsingenieria.udea.edu.co:8094/onomastico/images'
 };
