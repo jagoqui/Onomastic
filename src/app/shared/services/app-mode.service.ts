9799123a69f759d25722c710cea39e747476e3d0
Jaidiver Gómez Quintero <jaidiver.gomez@udea.edu.co>
2021-08-24 19:39:07 -0500

    Se crea modo desarrollor en la app, para hacer pruebas de mantenimiento.

---
+++ src/app/shared/services/app-mode.service.ts
@@ -0,0 +1,60 @@
+import {Injectable} from '@angular/core';
+import {environment} from '@env/environment';
+import SwAlert from 'sweetalert2';
+import Swal from 'sweetalert2';
+
+type MODES = 'local' | 'test' | 'production';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppModeService {
+
+  constructor() {
+    if (environment.production) {
+      environment.apiUrl = environment.modesApp.production;
+    } else {
+      environment.apiUrl = environment.modesApp.test;
+    }
+  }
+
+  set mode(mode: MODES) {
+    SwAlert.fire({
+      title: `Va a cambiar al modo ${mode.toUpperCase()}, está seguro que dese hacerlo? `,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, cambiar de modo!',
+      cancelButtonText: 'Cancelar'
+    }).then((resultDelete) => {
+      if (resultDelete.isConfirmed) {
+        SwAlert.fire({
+          title: 'Ingrese la contraseña de pruebas para poder efectuar el cambio.',
+          input: 'text',
+          inputAttributes: {
+            autocapitalize: 'off'
+          },
+          showCancelButton: true,
+          confirmButtonText: 'Aceptar',
+          showLoaderOnConfirm: true
+        }).then((result) => {
+          if (result.value === '123456') {
+            localStorage.clear();
+            Swal.fire({
+              icon: 'success',
+              title: 'Se cambio de modo exitosamente!',
+              confirmButtonText: 'Aceptar!'
+            }).then();
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: 'Error a ingresar crendeciales!',
+              confirmButtonText: 'Aceptar!'
+            }).then();
+          }
+        });
+      }
+    });
+  }
+}
