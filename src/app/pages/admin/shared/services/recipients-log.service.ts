2b8ce712b94ba5e4480bd1cf432961b178db544d
Jaidiver Gómez Quintero <jaidiver.gomez@udea.edu.co>
2021-09-20 13:35:54 -0500

    Se crea componente para gestionar la califición de los correos por parte de los destinatarios.

--- src/app/pages/admin/shared/services/recipients-log.service.ts
+++ src/app/pages/admin/shared/services/recipients-log.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from '@env/environment';
-import { RecipientsLog } from '@adminShared/models/recipients-log.model';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '@env/environment';
+import {RecipientsLog} from '@adminShared/models/recipients-log.model';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +15,37 @@ export class RecipientsLogService {
   getAll(): Observable<RecipientsLog[]> {
     return this.http
       .get<RecipientsLog[]>(`${environment.apiUrl}/emails`);
+  }
+
+  setLoveStatus(isLove: boolean, userEmail: string, eventId: string): Observable<RecipientsLog> {
+    const params = new HttpParams(
+      {
+        fromObject: {
+          isLove: isLove as unknown as string,
+          userEmail: userEmail as unknown as string,
+          eventId: eventId as unknown as string
+        }
+      }
+    );
+    return this.http
+      .get<RecipientsLog>(`${environment.apiUrl}/recipients_log/set_love`, {params});
+  }
+
+  getLoveStatus(userEmail: string, eventId: string): Observable<boolean> {
+    const params = new HttpParams(
+      {
+        fromObject: {
+          userEmail: userEmail as unknown as string,
+          eventId: eventId as unknown as string
+        }
+      }
+    );
+    return this.http
+      .get<boolean>(`${environment.apiUrl}/recipients_log/love_status`, {params});
+  }
+
+  sentFeedback(feedback: string): Observable<RecipientsLog> {
+    return this.http
+      .get<RecipientsLog>(`${environment.apiUrl}/recipients_log/feedback/${feedback}`);
   }
 }
---
+++ src/app/shared/components/love-mail-status/love-mail-status.component.ts
@@ -0,0 +1,98 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subject} from 'rxjs';
+import {ActivatedRoute, Router} from '@angular/router';
+import SwAlert from 'sweetalert2';
+import {takeUntil} from 'rxjs/operators';
+import {RecipientsLogService} from '@adminShared/services/recipients-log.service';
+
+@Component({
+  selector: 'app-love-mail-status',
+  template: ''
+})
+export class LoveMailStatusComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<any>();
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private recipientsLogSvc: RecipientsLogService
+  ) {
+  }
+
+  ngOnInit(): void {
+    const email = this.route.snapshot.paramMap.get('email');
+    const eventId = this.route.snapshot.paramMap.get('event_id');
+    console.log(email, eventId);
+
+    let isFavorite: boolean;
+    this.recipientsLogSvc.getLoveStatus(email, eventId).pipe(takeUntil(this.destroy$))
+      .subscribe((favorite) => isFavorite = favorite); //TODO: Hacer await
+
+    SwAlert.fire({
+      title: '¡Calificános! 😊.',
+      text: `${isFavorite ? 'Ya habías marcado cómo favorito tu correo, ya no te gusta?' : 'Si has llegado hasta aquí' +
+        ' es porque te gusta nuestro correo 😍.'}`,
+      icon: 'info',
+      showCloseButton: true,
+      confirmButtonColor: '#d33',
+      confirmButtonText: `${isFavorite ? 'Debemos mejorar. 🥺' : 'Sí, ¡Me encata! 🥰'}`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.recipientsLogSvc.setLoveStatus(!isFavorite, email, eventId)
+          .pipe(takeUntil(this.destroy$))
+          .subscribe((recipientLog) => {
+            if (recipientLog) {
+              SwAlert.fire({
+                icon:'success',
+                title: 'Gracias por calificarnos',
+                footer: 'Tu opinión es de muy importante para nosotros para logar a que cada vez tengas una mejor' +
+                  ' experiencia.',
+                input: 'text',
+                inputAttributes: {
+                  autocapitalize: 'true'
+                },
+                showCancelButton: true,
+                confirmButtonText: 'Enviar retroalimentación',
+                cancelButtonText:'No tengo nada por decir',
+                showLoaderOnConfirm: true,
+                preConfirm: (sentFeedback) => fetch(`//api.github.com/users/jagoqui`)
+                    .then(response => {
+                      if (!response.ok) {
+                        throw new Error(response.statusText);
+                      }
+                      return response.json();
+                    })
+                    .catch(error => {
+                      SwAlert.showValidationMessage(
+                        `Petición fallida, ${error}`
+                      );
+                    }),
+                allowOutsideClick: () => !SwAlert.isLoading()
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  SwAlert.fire({
+                    title: `${result.value.login}'s avatar`,
+                    imageUrl: result.value.avatar_url
+                  }).then();
+                }
+              });
+            } else {
+              SwAlert.fire(
+                '¡El usuario o evento asociado no existe!',
+                `
+                        Para más información
+                        <a href='${this.router.navigate(['PUBLISHER/help'])}'>Ayuda</a>.
+                    `,
+                'error'
+              ).then();
+            }
+          }, () => SwAlert.showValidationMessage('Error calificando el evento.'));
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next({});
+    this.destroy$.complete();
+  };
+}
